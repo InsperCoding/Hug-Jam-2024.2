3d9ef3d4df2e2203b015d9ba58222da5e466db6a
ribollequis87 <gustavocr2@al.insper.edu.br>
2024-08-24 16:49:01 -0300

    Teclas Inversas (I)

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -3,21 +3,25 @@ extends CharacterBody2D
 @onready var globals = get_node("/root/Globals")
 const NORMAL_SPEED : int = 300
 const SLOW_SPEED : int = 150
-const SLOW_DOWN_DURATION : float = 5.0 # Duração da lentidão em segundos
+const SLOW_DOWN_DURATION : float = 5.0 
 var SPEED : int = NORMAL_SPEED
 signal shoot()
 signal pickup(body : StaticBody2D)
 
 @onready var slow_down_timer : Timer = $SlowDownTimer
+@onready var key_swap_timer : Timer = $KeySwapTimer
 
 var penalidades = [] # Lista de penalidades aplicadas
 
 func _ready() -> void:
-	slow_down_timer = $SlowDownTimer
 	if slow_down_timer:
 		slow_down_timer.connect("timeout", Callable(self, "_on_slow_down_timeout"))
 	else:
 		print("Erro: SlowDownTimer não encontrado!")
+	if key_swap_timer:
+		key_swap_timer.connect("timeout", Callable(self, "_on_key_swap_timeout"))
+	else:
+		print("Erro: KeySwapTimer não encontrado!")
 
 func handle_inventory() -> void:
 	for i in range($Inventory.get_child_count()):
@@ -27,16 +31,21 @@ func handle_inventory() -> void:
 			$Inventory.get_child(i).visible = false
 
 func get_input():
-	var input_direction : Vector2 = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * SPEED
+	var input_direction : Vector2
+	if "trocar_teclas" in penalidades:
+		input_direction = Input.get_vector("right", "left", "down", "up")
+	else:
+		input_direction = Input.get_vector("left", "right", "up", "down")
 	
+	velocity = input_direction * SPEED
+
 	if Input.is_action_just_pressed("mouseleft"):
 		emit_signal("shoot")
 
 func _physics_process(delta):
 	handle_inventory()
 	get_input()
-	move_and_slide()
+	move_and_slide() # Chama move_and_slide() sem argumentos
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
@@ -50,7 +59,7 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("penalty1"):
 		aplicar_lentidao()
 	elif Input.is_action_just_pressed("penalty2"):
-		trocar_teclas_temporariamente()
+		alterar_teclas()
 	elif Input.is_action_just_pressed("penalty3"):
 		aplicar_spawn_policia()
 
@@ -61,21 +70,22 @@ func aplicar_lentidao():
 		SPEED = SLOW_SPEED
 		if slow_down_timer:
 			slow_down_timer.start(SLOW_DOWN_DURATION)
-		else:
-			print("Erro: SlowDownTimer não encontrado!")
-		print("Lentidão aplicada!")
 
 func _on_slow_down_timeout() -> void:
 	if "lentidao" in penalidades:
 		penalidades.erase("lentidao")
 		SPEED = NORMAL_SPEED
-		print("Lentidão removida!")
 
 # PENALIDADE 2
-func trocar_teclas_temporariamente():
+func alterar_teclas():
 	if "trocar_teclas" not in penalidades:
 		penalidades.append("trocar_teclas")
-		print("Teclas trocadas temporariamente!")
+		if key_swap_timer:
+			key_swap_timer.start(SLOW_DOWN_DURATION)
+
+func _on_key_swap_timeout() -> void:
+	if "trocar_teclas" in penalidades:
+		penalidades.erase("trocar_teclas")
 
 # PENALIDADE 3
 func aplicar_spawn_policia():
