36a0ba9d337a401ab433dfac15b5ac1cb7c426f1
Artur Cruz <crywatcher@gmail.com>
2024-08-24 14:30:04 -0300

    itens e cenario arrumados

---
+++ src/Globals.gd
@@ -0,0 +1,6 @@
+extends Node
+
+# CONSIDER + 1
+const INVENTORY_SIZE_MAX : int = 4
+
+var inventory_idx : int = 0
--- src/Player/player.gd
+++ src/Player/player.gd
@@ -1,9 +1,17 @@
 extends CharacterBody2D
 
 
-const SPEED : int = 100
+@onready var globals = get_node("/root/Globals")
+const SPEED : int = 300
 signal shoot()
+signal pickup(body : StaticBody2D)
 
+func handle_inventory() -> void:
+	for i in range($Inventory.get_child_count()):
+		if i == globals.inventory_idx:
+			$Inventory.get_child(i).visible = true
+		else:
+			$Inventory.get_child(i).visible = false
 func get_input():
 
 	# gets input vectors and applies it to the velocity
@@ -15,6 +23,24 @@ func get_input():
 		emit_signal("shoot")
 	
 func _physics_process(delta):
-
+	
+	
+	handle_inventory()
 	get_input()
 	move_and_slide()
+
+
+func _input(event: InputEvent) -> void:
+	
+	# Pickup items
+	if event.is_action_pressed("interact"):
+		# checks all bodies in the interaction area
+		for body in $InteractionArea.get_overlapping_bodies():
+			# if the body is already a child of player, skip
+			if body in get_children():
+				continue
+			if "Item" in body.name:
+				emit_signal("pickup", body)
+				
+				break
+			
