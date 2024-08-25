dad85c76ad0018d56b9fcb4dc6ef57c153034865
CYNahko <cynthiayasutake@gmail.com>
2024-08-24 23:42:48 -0300

    policial feito, mas zero npcs, sorry

---
+++ src/Officer/raio_de_verificação.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+signal isPolite_check
+
+func _init() -> void:
+	collision_layer = 1
+	collision_mask = 0
--- src/Player/player.gd
+++ src/Player/player.gd
@@ -6,11 +6,13 @@ const SLOW_DOWN_DURATION : float = 10.0
 var SPEED : int = NORMAL_SPEED
 signal shoot()
 signal pickup(body : StaticBody2D)
+signal isPolite
+signal hit
 
 @onready var slow_down_timer : Timer = $SlowDownTimer
 @onready var key_swap_timer : Timer = $KeySwapTimer
 
-var penalidades = [] 
+@export var penalidades = [] 
 
 var regras = [
 	"Fonte", #Jogar sujeira no rio/fonte ao invés de limpar
@@ -96,4 +98,25 @@ func _on_key_swap_timeout() -> void:
 func aplicar_spawn_policia():
 	if "spawn_policia" not in penalidades:
 		penalidades.append("spawn_policia")
+		#var new_police = Policia.instantiate()
+		var posx = randf_range(0,1152)
+		var posy = randf_range(0,648)
+		#new_police.position = 
+		#add_child(new_police)
 		print("Spawn de polícia aplicado!")
+		
+
+func is_polite():
+	pass
+
+
+func _on_hitbox_randomize_punishment():
+	var rng = randf_range(1,3)
+	if rng == 1:
+		aplicar_lentidao()
+		_on_slow_down_timeout()
+	elif rng == 2:
+		alterar_teclas()
+		_on_key_swap_timeout()
+	else:
+		aplicar_spawn_policia()
